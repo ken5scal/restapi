a8f6a2218418195b259b5003bb5b8d4d9b0dd2e9
ken5scal <kengoscal@gmail.com>
2016-07-27 02:07:53 +0900

    Updatd

--- restapi.go
+++ restapi.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"net/http"
-	"fmt"
 	"github.com/gin-gonic/gin"
 	"time"
 	"io/ioutil"
+
 )
+
+// usr gorm for ORMapper
 
 type Todo struct {
 	Name string `json:"name"`
@@ -42,7 +44,7 @@ func TodoPost(c *gin.Context) {
 func FileOutput(c *gin.Context) {
 	contents, err := ioutil.ReadFile("sample.json")
 	if err != nil {
-		fmt.Println(contents, err)
+		//fmt.Println(contents, err)
 		c.JSON(400, gin.H{"status": "bad request"})
 		return
 	}
@@ -50,20 +52,20 @@ func FileOutput(c *gin.Context) {
 	c.String(200, string(contents))
 }
 
-func handle(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello world!")
-}
+//func handle(w http.ResponseWriter, r *http.Request) {
+//	fmt.Fprint(w, "Hello world!")
+//}
 
-func main() {
+func init() {
 	//http.HandleFunc("/", handle)
 	//fmt.Printf("Start GO HTTP Server")
 	//http.ListenAndServe(":8080", nil)
-	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 	r.GET("/", Index)
 	r.GET("/todos", TodoIndex)
 	r.POST("/todo", TodoPost)
 	r.GET("/file", FileOutput)
-	r.Run(":8080")
-	fmt.Println("server start port 8080")
+	//r.Run(":8080")
+	//fmt.Println("server start port 8080")
+	http.Handle("/", r)
 }
